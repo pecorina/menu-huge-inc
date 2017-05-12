73444e45253ec884145873316ce48663d7464536
pecorina <constanza.pecori@gmail.com>
2017-05-11 22:45:10 -0300

    intento fallido

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,21 @@ document.getElementById("toggle").addEventListener("click", function(event){
 	document.getElementById("nav-header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden");
 });
+
+//var currentScroll = window.pageYOffset || document.body.scrollTop; 
+// con esto nos devolvera la posicion de scroll. scroll?
+var lastScrollTop=0;
+window.addEventListener("scroll", function(){
+	var currentScroll = window.pageYOffset || document.body.scrollTop;
+	if (currentScroll > lastScrollTop) {
+		console.log("down");
+	}else{
+		console.log("up");
+	}
+	lastScrollTop = currentScroll;
+});
+//hacemos nuestra primera comparacion y debuggeamos el resultado
+
+nav.header-in{
+	background-color:rgba(217, 40, 130, 0.9);
+}
